2597742ddd76c5751ec42dbd8cb6d85e304c8961
Fábio Henrique <fabiohenriquesf8@gmail.com>
2025-07-29 22:33:33 -0300

    feat: added a simple hash comparission

--- src/handlers/auth-handlers.ts
+++ src/handlers/auth-handlers.ts
@@ -1,6 +1,7 @@
 ﻿import { Context } from "koa";
 import { badRequest, ERROR_CODES } from "../middlewares/error-middleware";
 import { userModel } from "../schemas/user";
+import hashUtils from "../utils/hash";
 
 interface ILogin {
   email: string;
@@ -15,10 +16,16 @@ const signinHandler = async (ctx: Context) => {
     throw badRequest(ERROR_CODES.BAD_INPUT);
   }
 
-  const user = await userModel.findOne({ email, password });
+  const user = await userModel.findOne({ email });
 
   if (!user) {
-    throw badRequest(ERROR_CODES.BAD_REQUEST, "Email ou senha incorretos.");
+    throw badRequest(ERROR_CODES.BAD_REQUEST, "Email incorreto.");
+  }
+
+  const isSamePwd = hashUtils.compareString(password, user.password);
+
+  if (!isSamePwd) {
+    throw badRequest(ERROR_CODES.BAD_REQUEST, "Senha incorreta.");
   }
 
   return ctx;
