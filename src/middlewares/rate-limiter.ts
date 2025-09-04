da098cc8ee873f30e5f26462f6b4e9413d07b096
Fábio Henrique <fabiohenriquesf8@gmail.com>
2025-09-03 22:56:38 -0300

    feat: added function to check user tokens

--- src/middlewares/rate-limiter.ts
+++ src/middlewares/rate-limiter.ts
@@ -1,1 +1,19 @@
-﻿
+﻿import { Context, Next } from "koa";
+
+import { ERROR_CODES, tooManyRequests } from "../utils/errors";
+
+import { IJwtClaims } from "../utils/jwt";
+
+import cacheService from "../services/cache-service";
+import cacheUtils from "../utils/cache";
+
+export const checkUserTokens = async (ctx: Context) => {
+  const user: IJwtClaims = ctx.state.user;
+
+  const key = cacheUtils.mountUserTokenKey(user._id.toString());
+  const tokens = await cacheService.getCacheValue<number>(key);
+
+  if (tokens <= 0) {
+    throw tooManyRequests(ERROR_CODES.TOO_MANY_REQUESTS, "Insuficient tokens.");
+  }
+};
