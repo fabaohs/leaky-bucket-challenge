defe881e85c1d2095b646b620c626aa82beb8985
Fábio Henrique <fabiohenriquesf8@gmail.com>
2025-09-03 23:24:55 -0300

    feat: added token consuming logic

---
+++ src/services/bucket-service.ts
@@ -0,0 +1,17 @@
+﻿import cacheService from "./cache-service";
+import cacheUtils from "../utils/cache";
+
+const consumeToken = async (userId: string) => {
+  const key = cacheUtils.mountUserTokenKey(userId);
+  const tokens = await cacheService.getCacheValue<number>(key);
+
+  if (!tokens || tokens <= 0) {
+    return;
+  }
+
+  return await cacheService.decreaseTokens(key);
+};
+
+export default {
+  consumeToken,
+};
